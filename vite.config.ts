e23afe8b47ba4a3ec0c84925cdcb164b25eb8437
Ridham Panchal <ridd.panchal26@gmail.com>
2026-03-13 14:52:29 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // server: {
+    // host: '10.55.2.44',  // allows access from other devices
+    // port: 5173,       // optional, keeps it consistent
+  // }
+})
